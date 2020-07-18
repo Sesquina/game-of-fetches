2a4ff779962e81158cbed5e8d34c697cd4dcd7ff
Samira Esquina <samiraesquina@gmail.com>
2020-07-17 22:41:54 -0400

    Fixed Bugs

---
+++ Desktop/cassini/mod2/w2/game-of-fetches/src/components/ThirdQuestion.js
@@ -0,0 +1,40 @@
+import React from "react";
+import axios from "axios";
+const URL = "https://anapioficeandfire.com/api/";
+
+class ThirdQuestion extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            info: []
+        }
+    }
+
+    async getAnswer() {
+        try {
+            const response = await axios.get(URL + "houses/229")
+            console.log(response.data)
+            this.setState({
+                info: response.data
+            });
+
+        } catch {
+
+        }
+    }
+
+    componentDidMount() {
+        this.getAnswer()
+    }
+    render() {
+        return (
+            <div>
+                <h1>What is the coat of arms for House Lannister?</h1>
+                <h2>{this.state.info.coatOfArms}</h2>
+            </div>
+        )
+    }
+
+}
+
+export default ThirdQuestion;
